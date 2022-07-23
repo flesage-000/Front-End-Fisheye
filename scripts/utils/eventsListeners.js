6018c7bb5550f13bead9fb4d59d5494acaeeb8b5
flesage-000 <flesage.spam@gmail.com>
2022-07-23 18:01:39 +0200

    Add comments

--- scripts/utils/eventsListeners.js
+++ scripts/utils/eventsListeners.js
@@ -1,23 +1,25 @@
 // Tutorial => https://www.youtube.com/watch?v=TZh_AiOuBr8
 
+/**
+ * Observable class
+ */
 class eventsListeners {
   constructor () {
     this.listeners = [];
   }
 
   addListener(callback) {
-    // console.log('addListener', callback);
     this.listeners.push(callback);
   }
 
   setEvent(callback) {
-    console.log('setEvent', callback);
     this.listener = callback;
   }
 
   ifListener() {
     if(this.listeners.length > 0) {
       for(let listener of this.listeners) {
+        // Because added event are function we can call listener var as a function
         listener();
       }
     }
