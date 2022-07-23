9eb1c20eb0e137fa5c6fff619ebe8a63683f0762
flesage-000 <flesage.spam@gmail.com>
2022-07-23 06:33:58 +0200

    add fex aria

--- scripts/Templates/MediaCard.js
+++ scripts/Templates/MediaCard.js
@@ -9,11 +9,11 @@ class MediaCard {
   createImageCard(index) {
     const $wrapper = document.createElement('article');
     const mediaCard = `
-      <a href="#" title="${this._media.title}, close up view" class="lightboxOpener"><img src="${this._media.image}" class="articles__media__img"></a>
+      <a href="#" title="${this._media.title}, close up view" class="lightboxOpener"><img src="${this._media.image}" class="articles__media__img" title="${this._media.title}, close up view"></a>
       <span class="articles__media__name">${this._media.title}</span>
       <span class="articles__media__like">
         <span class="articles__media__like__total">${this._media.likes}</span>
-        <a href="#" class="articles__media__like__heart"></a>
+        <span class="articles__media__like__heart"></span>
       </span>
     `;
 
@@ -38,7 +38,7 @@ class MediaCard {
       <span class="articles__media__name">${this._media.title}</span>
       <span class="articles__media__like">
         <span class="articles__media__like__total">${this._media.likes}</span>
-        <a href="#" class="articles__media__like__heart"></a>
+        <span class="articles__media__like__heart"></span>
       </span>
     `;
 
